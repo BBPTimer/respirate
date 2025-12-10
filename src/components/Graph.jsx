70c31b2ce6fae970a2272c193b20b6252f3aed25
BBPTimer <greg.weseloh@gmail.com>
2025-12-10 09:13:19 -0600

    Added date range to Graph component

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,10 +1,37 @@
 import { LineChart } from "@mui/x-charts/LineChart";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { formatDate } from "../common/utils";
 import { AppContext } from "../contexts/AppContext";
 
 const Graph = () => {
   const { targetRate, setTargetRate, rates } = useContext(AppContext);
+
+  const [data, setData] = useState([...rates]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    let startDate = new Date();
+    let endDate = new Date();
+
+    if (event.target.startDate.value) {
+      startDate = new Date(event.target.startDate.value);
+    }
+
+    if (event.target.endDate.value) {
+      endDate = new Date(event.target.endDate.value);
+    }
+
+    setData(
+      rates.filter(
+        (rate) => rate.timestamp >= startDate && rate.timestamp <= endDate
+      )
+    );
+  };
+
+  const showAllData = () => {
+    setData([...rates]);
+  };
 
   const valueFormatter = (value) => {
     const dateObject = new Date(value);
@@ -27,8 +54,22 @@ const Graph = () => {
         />{" "}
         breaths/minute
       </i>
+      <br />
+      <br />
+
+      <form onSubmit={handleSubmit}>
+        <h5>Date Range</h5>
+        <label htmlFor="startDate">Start date: </label>
+        <input type="date" id="startDate" name="startDate" />{" "}
+        <label htmlFor="endDate">End date: </label>
+        <input type="date" id="endDate" name="endDate" /> <button>Save</button>
+        <button type="button" onClick={() => setData([...rates])}>
+          Show All Data
+        </button>
+      </form>
+
       <LineChart
-        dataset={rates}
+        dataset={data}
         xAxis={[
           {
             dataKey: "timestamp",
